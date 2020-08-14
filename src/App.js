4113313fe63b04f358631f6a9446e6284c53dcce
MarselBurdo <9003363@gmail.com>
2020-08-14 14:28:10 +0300

    magic buttom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import Home from './Componets/Home/Home';
+import React from "react";
+import Home from "./Componets/Home/Home";
+import { useSelector } from "react-redux";
 
+import { makeStyles } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  // paper: {
+  //   padding: theme.spacing(2),
+  //   textAlign: "center",
+  //   color: "#a0c4ff",
+  // },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  magic: {
+    backgroundColor: 'black',
+  }
+}));
 
 function App() {
-  return (<>
-<Home />
-  </>);
+  const magic = useSelector((state) => state.action.magic);
+  const classes = useStyles();
+  return (
+    <>
+    <Grid className={(magic) ?classes.root: classes.magic}>
+      <Home />
+     
+    </Grid>
+    </>
+  );
 }
 
 export default App;
