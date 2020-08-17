645e5a6d2fe3e6eba34ae64661ab04ec904fef07
Mikhail <estrade77@gmail.com>
2020-08-17 18:19:12 +0300

    Table Usa Shares

--- src/Componets/redux/actionTypes.js
+++ src/Componets/redux/actionTypes.js
@@ -2,3 +2,6 @@ export const THIS_ACTION = 'THIS_ACTION'
 export const NEWS_ADD='NEWS_ADD'
 export const STOCK_START = 'STOCK_START'
 export const STOCK_ADD = 'STOCK_ADD'
+
+//table with top-5 USA stocks
+export const STOCK_TABLE_ADD = 'STOCK_TABLE_ADD'
